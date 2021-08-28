fb771f69736fb1eaa03b3b222a855ad3abba5ae1
Eaeajj <eccentricmadm@gmail.com>
2021-08-28 14:51:28 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 5056
+
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+async function start() {
+  try {
+    await mongoose.connect(
+      "THERE SHOULD BE A LINK TO THE MONGO DB",
+      {
+        useNewUrlParser: true,
+        useFindAndModify: false
+      }
+    )
+    app.listen(PORT, () => {
+      console.log('Server has been started...')
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
